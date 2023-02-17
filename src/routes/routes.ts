27a47ed9f705657d88f02f514e15403a3320f92d
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-17 17:47:32 -0300

    Uncomment route to add a new ad

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 // import { findAllGames, findById, createNewAd, getDiscordById } from "../controllers/AdsController";
 
-import { findAllGames } from "../controllers/AdsController";
+import { findAllGames, createNewAd } from "../controllers/AdsController";
 
 const routes = Router();
 
 routes.get('/games', findAllGames);
-// routes.post('/games/:id/ads', createNewAd);
+routes.post('/games/:id/ads', createNewAd);
 // routes.get('/ganes/:id/ads', findById);
 // routes.get('/ads/:id/discord', getDiscordById);
 
